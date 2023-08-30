f018bb71c6f7a230a7bcf1403e7f176e992387f1
Daniel de Andrade Varela <1902294+drachehavoc@users.noreply.github.com>
2023-08-30 20:04:34 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # 2023-IFC-IA22-React
+
+## Criação do Projeto
+
+```sh
+ npm create vite@latest
+ ```
+
+ ## Execução do Projeto
+
+```sh
+cd __NOME_DA_PASTA_DO_PROJETO__
+npm run dev
+```
+
+## Sincronizar com a Origem 
+
+```sh
+git add --all
+```
---
+++ vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>
+        count is {count}
+      </button>
+  </>
+}
+
+export default App
